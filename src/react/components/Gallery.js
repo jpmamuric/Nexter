c36b7d20aa6b0a863aa8a911415a47962a796236
Jan Peter Mamuric <webdev.jmamuric@gmail.com>
2018-05-07 08:52:45 -0700

    added gallery layout

--- src/react/components/Gallery.js
+++ src/react/components/Gallery.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
+const galleryitems = [ '1','2','3','4','5','6','7','8','9','10','11','12','13','14'];
+
 const Gallery = () => (
   <section className="gallery">
-    Gallery
+
+    { galleryitems.map( item =>(
+      <figure key={item} className={`gallery__item gallery__item--${item}`}>
+        <img
+          alt="gallery"
+          className="gallery__img"
+          src={require(`../../assets/img/gal-${item}.jpeg`)} />
+      </figure>
+
+    ))}
   </section>
 );
 
